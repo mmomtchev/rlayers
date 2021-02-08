4766f1a87d003b8282e7ae34f76f5abd29d1b310
Momtchil Momtchev <momtchil@momtchev.com>
2021-02-08 20:06:45 +0100

    fix event names

--- src/Map.tsx
+++ src/Map.tsx
@@ -22,9 +22,9 @@ export interface MapProps {
     onPointerDrag?: (e: MapBrowserEvent) => boolean | void;
     onPointerMove?: (e: MapBrowserEvent) => boolean | void;
     onPostRender?: (e: MapEvent) => boolean | void;
-    OnPreCompose?: (e: RenderEvent) => boolean | void;
-    OnPostCompose?: (e: RenderEvent) => boolean | void;
-    OnRenderComplete?: (e: RenderEvent) => boolean | void;
+    onPreCompose?: (e: RenderEvent) => boolean | void;
+    onPostCompose?: (e: RenderEvent) => boolean | void;
+    onRenderComplete?: (e: RenderEvent) => boolean | void;
 }
 
 export const MapContext = React.createContext(null);
