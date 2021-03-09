9d5d4687fe964c7b33c7eb965eebb42acf0188ca
Momtchil Momtchev <momtchil@momtchev.com>
2021-03-09 14:41:34 +0100

    restore the custom module definitions

---
+++ examples/custom.d.ts
@@ -0,0 +1,14 @@
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.igc' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.geojson' {
+    const content: string;
+    export default content;
+}
