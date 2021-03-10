8e3c22f0846d625227307dca89e58b5daf82c62b
Momtchil Momtchev <momtchil@momtchev.com>
2021-03-10 21:56:13 +0100

    lazy load all the examples

--- examples/App.tsx
+++ examples/App.tsx
@@ -6,66 +6,41 @@ import React from 'react';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import {Button} from 'react-bootstrap';
 
-import Simple from './Simple';
-// @ts-ignore
-import SimpleJSX from '!!raw-loader!./Simple.tsx';
-import Layers from './Layers';
-// @ts-ignore
-import LayersJSX from '!!raw-loader!./Layers.tsx';
-import Controls from './Controls';
-// @ts-ignore
-import ControlsJSX from '!!raw-loader!./Controls.tsx';
-import Overlays from './Overlays';
-// @ts-ignore
-import OverlaysJSX from '!!raw-loader!./Overlays.tsx';
-import Popups from './Popups';
-// @ts-ignore
-import PopupsJSX from '!!raw-loader!./Popups.tsx';
-import Features from './Features';
-// @ts-ignore
-import FeaturesJSX from '!!raw-loader!./Features.tsx';
-import PinDrop from './PinDrop';
-// @ts-ignore
-import PinDropJSX from '!!raw-loader!./PinDrop.tsx';
-import Reprojection from './Reprojection';
-// @ts-ignore
-import ReprojectionJSX from '!!raw-loader!./Reprojection.tsx';
-import IGC from './IGC';
-// @ts-ignore
-import IGCJSX from '!!raw-loader!./IGC.tsx';
-import Heatmap from './Heatmap';
-// @ts-ignore
-import HeatmapJSX from '!!raw-loader!./Heatmap.tsx';
-import Cluster from './Cluster';
-// @ts-ignore
-import ClusterJSX from '!!raw-loader!./Cluster.tsx';
-import Overview from './Overview';
-// @ts-ignore
-import OverviewJSX from '!!raw-loader!./Overview.tsx';
-import VectorTiles from './VectorTiles';
-// @ts-ignore
-import VectorTilesJSX from '!!raw-loader!./VectorTiles.tsx';
-import Interactions from './Interactions';
-// @ts-ignore
-import InteractionsJSX from '!!raw-loader!./Interactions.tsx';
-import Spinner from './Spinner';
-// @ts-ignore
-import SpinnerJSX from '!!raw-loader!./Spinner.tsx';
-import Extent from './Extent';
-// @ts-ignore
-import ExtentJSX from '!!raw-loader!./Extent.tsx';
-import Animation from './AnimatedOverlay';
-// @ts-ignore
-import AnimationJSX from '!!raw-loader!./AnimatedOverlay.tsx';
-import GeoData from './GeoData';
-// @ts-ignore
-import GeoDataJSX from '!!raw-loader!./GeoData.tsx';
-import Draw from './Draw';
-// @ts-ignore
-import DrawJSX from '!!raw-loader!./Draw.tsx';
-import Geolocation from './Geolocation';
-// @ts-ignore
-import GeolocationJSX from '!!raw-loader!./Geolocation.tsx';
+const examples = {
+    simple: {title: 'Simple map', file: 'Simple'},
+    controls: {title: 'Custom controls', file: 'Controls'},
+    overlays: {title: 'Simple overlay', file: 'Overlays'},
+    extent: {title: 'Constrained view', file: 'Extent'},
+    animation: {title: 'Animated icon', file: 'AnimatedOverlay'},
+    overview: {title: 'Overview', file: 'Overview'},
+    popups: {title: 'Popups', file: 'Popups'},
+    features: {title: 'GeoJSON', file: 'Features'},
+    cluster: {title: 'Clustering', file: 'Cluster'},
+    heatmap: {title: 'Heatmap', file: 'Heatmap'},
+    layers: {title: 'Multiple layers', file: 'Layers'},
+    pindrop: {title: 'Drop a pin', file: 'PinDrop'},
+    spinner: {title: 'Spinner', file: 'Spinner'},
+    geodata: {title: 'Infographics', file: 'GeoData'},
+    interactions: {title: 'Move & Select', file: 'Interactions'},
+    draw: {title: 'Draw & Modify', file: 'Draw'},
+    geolocation: {title: 'Geolocation', file: 'Geolocation'},
+    vectortiles: {title: 'Vector tiles', file: 'VectorTiles'},
+    reproj: {title: 'Reprojection', file: 'Reprojection'},
+    igc: {title: 'Performance', file: 'IGC'}
+};
+
+// These two bring in huge bundles and are lazy-loaded
+const ReadmeBlock = React.lazy(() => import(/* webpackPrefetch: true */ './ReadmeBlock'));
+const CodeBlock = React.lazy(() => import(/* webpackPrefetch: true */ './CodeBlock'));
+
+for (const ex of Object.keys(examples)) {
+    examples[ex].comp = React.lazy(
+        () => import(/* webpackPrefetch: true */ `./${examples[ex].file}.tsx`)
+    );
+    examples[ex].code = import(
+        /* webpackPrefetch: true */ `!!raw-loader!./${examples[ex].file}.tsx`
+    ).then((code) => code.default);
+}
 
 const LeftMenuItem = (props): JSX.Element => (
     <Link to={props.id}>
@@ -74,33 +49,6 @@ const LeftMenuItem = (props): JSX.Element => (
         </Button>
     </Link>
 );
-
-const examples = {
-    simple: {title: 'Simple map', comp: Simple, code: SimpleJSX},
-    controls: {title: 'Custom controls', comp: Controls, code: ControlsJSX},
-    overlays: {title: 'Simple overlay', comp: Overlays, code: OverlaysJSX},
-    extent: {title: 'Constrained view', comp: Extent, code: ExtentJSX},
-    animation: {title: 'Animated icon', comp: Animation, code: AnimationJSX},
-    overview: {title: 'Overview', comp: Overview, code: OverviewJSX},
-    popups: {title: 'Popups', comp: Popups, code: PopupsJSX},
-    features: {title: 'GeoJSON', comp: Features, code: FeaturesJSX},
-    cluster: {title: 'Clustering', comp: Cluster, code: ClusterJSX},
-    heatmap: {title: 'Heatmap', comp: Heatmap, code: HeatmapJSX},
-    layers: {title: 'Multiple layers', comp: Layers, code: LayersJSX},
-    pindrop: {title: 'Drop a pin', comp: PinDrop, code: PinDropJSX},
-    spinner: {title: 'Spinner', comp: Spinner, code: SpinnerJSX},
-    geodata: {title: 'Infographics', comp: GeoData, code: GeoDataJSX},
-    interactions: {title: 'Move & Select', comp: Interactions, code: InteractionsJSX},
-    draw: {title: 'Draw & Modify', comp: Draw, code: DrawJSX},
-    geolocation: {title: 'Geolocation', comp: Geolocation, code: GeolocationJSX},
-    vectortiles: {title: 'Vector tiles', comp: VectorTiles, code: VectorTilesJSX},
-    reproj: {title: 'Reprojection', comp: Reprojection, code: ReprojectionJSX},
-    igc: {title: 'Performance', comp: IGC, code: IGCJSX}
-};
-
-// These two bring in huge bundles and are lazy-loaded
-const ReadmeBlock = React.lazy(() => import('./ReadmeBlock'));
-const CodeBlock = React.lazy(() => import('./CodeBlock'));
 
 const App = (): JSX.Element => {
     return (
@@ -127,7 +75,11 @@ const App = (): JSX.Element => {
                         {Object.keys(examples).map((e) => (
                             <Route key={e} path={`/${e}`}>
                                 <div className='row'>
-                                    <div className='col-12 col-xl-5'>{examples[e].comp()}</div>
+                                    <div className='col-12 col-xl-5 mb-1'>
+                                        <React.Suspense fallback={<div>Loading component...</div>}>
+                                            {React.createElement(examples[e].comp)}
+                                        </React.Suspense>
+                                    </div>
                                     <div className='col-12 col-xl-7 codeblock'>
                                         <React.Suspense fallback={<div>Parsing code...</div>}>
                                             <CodeBlock code={examples[e].code} />
--- examples/CodeBlock.tsx
+++ examples/CodeBlock.tsx
@@ -5,10 +5,12 @@ import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import {darcula as styleHighlighter} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 // This is expensive to render
-const CodeBlock = React.memo(function _CodeBlock(props: {code: string}) {
+const CodeBlock = React.memo(function _CodeBlock(props: {code: Promise<string>}) {
+    const [code, setCode] = React.useState('loading()');
+    props.code.then((r) => setCode(r));
     return (
         <SyntaxHighlighter language='tsx' style={styleHighlighter}>
-            {prettier.format(props.code, {
+            {prettier.format(code, {
                 parser: 'typescript',
                 plugins: [parserTypescript]
             })}
