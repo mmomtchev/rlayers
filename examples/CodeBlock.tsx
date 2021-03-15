9d5d8e329b77b98a27702a840d17f4a3d0489baa
Momtchil Momtchev <momtchil@momtchev.com>
2021-03-15 11:48:46 +0100

    match the default VS code theme

--- examples/CodeBlock.tsx
+++ examples/CodeBlock.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import 'prism-themes/themes/prism-darcula.css';
+import 'prism-themes/themes/prism-vsc-dark-plus.css';
 
 // This is expensive to render
 const CodeBlock = React.memo(function _CodeBlock(props: {code: Promise<string>}) {
     const [code, setCode] = React.useState('loading()');
     props.code.then((r) => setCode(r));
     return (
-        <pre
-            style={{backgroundColor: 'rgb(83, 83, 83)'}}
-            dangerouslySetInnerHTML={{__html: code}}
-        />
+        <pre className='p-2' style={{backgroundColor: 'rgb(43, 43, 43)', fontSize: '16px'}}>
+            <code className='language-tsx' dangerouslySetInnerHTML={{__html: code}} />
+        </pre>
     );
 });
 
