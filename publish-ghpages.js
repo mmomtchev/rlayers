04ca4547c863c937511b83e4e8af7b03b677c10f
Momtchil Momtchev <momtchil@momtchev.com>
2021-04-09 10:43:29 +0200

    switch to gh-pages for publishing the docs

---
+++ publish-ghpages.js
@@ -0,0 +1,14 @@
+const ghpages = require('gh-pages');
+const path = require('path');
+
+process.stdout.write('Publishing to "gh-pages" branch... ');
+ghpages.publish(path.resolve(__dirname, 'docs'), {message: 'Updated documentation'}, function(err) {
+	if (err) {
+		process.stdout.write('error\n');
+		console.error(err);
+		process.exit(1);
+	} else {
+		process.stdout.write('success\n');
+		process.exit(0);
+	}
+});
