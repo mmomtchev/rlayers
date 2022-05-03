2fc08c69924144e80e948554041ebdc43010c338
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-03 15:17:29 +0200

    don't do async ops in render()

--- examples/CodeBlock.tsx
+++ examples/CodeBlock.tsx
@@ -4,7 +4,9 @@ import 'prism-themes/themes/prism-vsc-dark-plus.css';
 // This is expensive to render
 const CodeBlock = React.memo(function _CodeBlock(props: {code: Promise<string>}) {
     const [code, setCode] = React.useState('loading()');
-    props.code.then((r) => setCode(r));
+    React.useEffect(() => {
+        props.code.then((r) => setCode(r));
+    }, [props.code]);
     return (
         <pre className='p-2' style={{backgroundColor: 'rgb(43, 43, 43)', fontSize: '16px'}}>
             <code className='language-tsx' dangerouslySetInnerHTML={{__html: code}} />
