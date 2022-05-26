3d960630f80226953d02d72f0ac727452242b158
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-26 17:37:39 +0200

    update the docs path

--- scripts/publish-ghpages.js
+++ scripts/publish-ghpages.js
@@ -2,13 +2,17 @@ const ghpages = require('gh-pages');
 const path = require('path');
 
 process.stdout.write('Publishing to "gh-pages" branch... ');
-ghpages.publish(path.resolve(__dirname, 'docs'), {message: 'Updated documentation'}, function(err) {
-	if (err) {
-		process.stdout.write('error\n');
-		console.error(err);
-		process.exit(1);
-	} else {
-		process.stdout.write('success\n');
-		process.exit(0);
-	}
-});
+ghpages.publish(
+    path.resolve(__dirname, '..', 'docs'),
+    {message: 'Updated documentation'},
+    function (err) {
+        if (err) {
+            process.stdout.write('error\n');
+            console.error(err);
+            process.exit(1);
+        } else {
+            process.stdout.write('success\n');
+            process.exit(0);
+        }
+    }
+);
