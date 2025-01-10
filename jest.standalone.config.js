7c13f5d249d364b358cfe2b6cb02dbfd8399c3b5
Momtchil Momtchev <momtchil@momtchev.com>
2025-01-10 16:18:49 +0100

    update the standalone config

--- jest.standalone.config.js
+++ jest.standalone.config.js
@@ -19,13 +19,13 @@ module.exports = {
         ]
     },
     transformIgnorePatterns: [
-        '/node_modules/(?!(ol|txml|geotiff|quick-lru|color-|rbush|earcut|pbf|quickselect))'
+        '/node_modules/(?!(ol|txml|geotiff|quick-lru|color-|rbush|earcut|pbf|quickselect|node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill))'
     ],
     moduleNameMapper: {
         '\\.(css|less)$': '<rootDir>/test/css.js',
         '^txml/txml$': '<rootDir>/node_modules/txml/dist/txml'
     },
-    setupFiles: ['jest-canvas-mock'],
+    setupFiles: ['jest-canvas-mock', 'jsdom-worker'],
     setupFilesAfterEnv: ['@testing-library/jest-dom', './test/setup_mocks.ts'],
     testRegex: '/test/.*\\.test\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
